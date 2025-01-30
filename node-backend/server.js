a03f0de41375af8a1f4a94188241d3306096e229
John Adeyemo <ajohnofficial2020@gmail.com>
2025-01-30 01:11:44 -0600

    backend vercel file added

--- node-backend/server.js
+++ node-backend/server.js
@@ -10,4 +10,13 @@ app.use(bodyParser.json());
 app.use("/api/mutualfunds", mutualFundRoutes);
 
 const PORT = 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello from Express on Vercel!" });
+});
+
+// Export Express app for Vercel
+module.exports = app;

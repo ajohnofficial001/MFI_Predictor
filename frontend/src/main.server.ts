2b3a89092543f7b116df84d13d02202ae9d3af00
John Adeyemo <ajohnofficial2020@gmail.com>
2025-01-30 00:43:30 -0600

    Trimming unnecessary branch

--- frontend/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { AppModule } from './app/app.module';
-
-const bootstrap = () => bootstrapApplication(AppComponent);
-
-export default bootstrap;
